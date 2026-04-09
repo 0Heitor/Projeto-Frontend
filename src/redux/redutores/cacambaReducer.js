cc89bacb2d1783713b41ac35178ac0981b9b273c
0Heitor <heitortanizaka@terra.com.br>
2026-04-09 01:56:19 -0300

    commit

--- src/redux/redutores/cacambaReducer.js
+++ src/redux/redutores/cacambaReducer.js
@@ -1,8 +1,8 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import ESTADO from '../../recursos/estado';
-
-let urlBaseConsulta = import.meta.env.VITE_API_URL+'/api/cacambas/consulta';
-let urlBase = import.meta.env.VITE_API_URL+'/api/cacambas';
+const API_URL = import.meta.env.VITE_API_URL;
+let urlBaseConsulta = API_URL+'/api/cacambas/consulta';
+let urlBase = API_URL+'/api/cacambas';
 
 export const buscarCacambas = createAsyncThunk('cacambas/buscarCacambas', async (cacamba) => {
     try{
